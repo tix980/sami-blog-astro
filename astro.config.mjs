f79286e20ab4c41c6213b1b60c3a11045cc93801
tingwei xie <tix980@mail.usask.ca>
2023-11-14 22:30:35 -0500

    more updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  site:'https://samixie.com',
-  integrations: [tailwind(), sitemap(
-    {
-      filter: (page) => !/https:\/\/samixie\.com\/blog\/[0-9]+/.test(page),
-
-  }
-  )]
+  site: 'https://samixie.com',
+  integrations: [tailwind(), sitemap({
+    filter: page => !/https:\/\/samixie\.com\/blog\/[0-9]+/.test(page)
+  })],
+  output: "hybrid",
+  adapter: node({
+    mode: "standalone"
+  })
 });
