d9ddba953da367936c1a24f2461e19c5e466ef10
tingwei xie <tix980@mail.usask.ca>
2023-11-18 19:04:17 -0500

    og images

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,5 +13,10 @@ export default defineConfig({
   output: "hybrid",
   adapter: node({
     mode: "standalone"
-  })
+  }),
+  vite: {
+    optimizeDeps: {
+      exclude: ['@resvg/resvg-js'],
+    },
+      },
 });
