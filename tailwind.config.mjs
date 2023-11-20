0ee00d21a5307b31681f99bc8bfcca6bec773b9b
tingwei xie <tix980@mail.usask.ca>
2023-11-19 21:47:43 -0500

    import tailwind css

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,31 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {colors: {
+			'text': 'var(--text)',
+			'background': 'var(--background)',
+			'primary': 'var(--primary)',
+			'secondary': 'var(--secondary)',
+		'accent': 'var(--accent)',
+		},fontSize: {
+			sm: '0.750rem',
+			base: '1rem',
+			xl: '1.333rem',
+			'2xl': '1.777rem',
+			'3xl': '2.369rem',
+			'4xl': '3.158rem',
+			'5xl': '4.210rem',
+		},
+		fontFamily: {
+			heading: 'Kdam Thmor Pro',
+			body: 'Open Sans',
+		},
+		fontWeight: {
+			normal: '400',
+			bold: '700',
+		},},
 	},
 	plugins: [],
+
+	
 }
